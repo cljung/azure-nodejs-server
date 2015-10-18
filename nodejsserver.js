ecd669e30bc76fc6e673131f2f36cca9f89f0204
Christer Ljung <cljung@users.noreply.github.com>
2015-10-18 17:45:35 +0200

    Create nodejsserver.js

---
+++ nodejsserver.js
@@ -0,0 +1,29 @@
+// on an Ubunto 14.04 Linux server in Azure, do:
+// sudo apt-get install nodejs
+// sudo apt-get install npm
+// sudo nodejs nodejsserver.js &
+
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+  return '0.0.0.0';
+}
+var ipaddr = getIPAddress();
+var http = require('http');
+var port = 80;
+var server = http.createServer(function(req, res) {
+        var dtf = new Date().toISOString();
+        res.writeHead(200);
+        res.write('<h1>Request processed by server ' + ipaddr + '</h1>' + '<br/>' + dtf);
+        res.end();
+});
+server.listen(port, function() {
+        console.log('server ' + ipaddr + ' listening on port ' + port);
+});
