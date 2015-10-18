f795a3d9a75c5b82f838ac9f7ba438d72e690f34
Christer Ljung <cljung@users.noreply.github.com>
2015-10-18 17:46:53 +0200

    Create ubuntu-install-nodejs.sh

---
+++ ubuntu-install-nodejs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Installing node.js and starting webserver"
+
+# ---------------------------------------------------------
+# install node.js and node.js package manager
+# ---------------------------------------------------------
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
+
+# ---------------------------------------------------------
+# Start the node.js webserver
+# ---------------------------------------------------------
+cd ~/azure-nodejs-demoserver
+sudo nodejs nodejsserver.js &
